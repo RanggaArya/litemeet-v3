d8d40cb5a5595859dd70381f9e152255255c9a4f
RanggaArya <dinainggrid@gmail.com>
2026-06-03 10:42:15 +0700

    style(mobile): optimize PiP size and use object-fit contain to prevent cropping

--- mobile/src/App.jsx
+++ mobile/src/App.jsx
@@ -182,7 +182,7 @@ function SmartVideoLayout({ tracks, remoteCount, isPipMode }) {
             : <div className="waiting-room"><div className="waiting-icon">👥</div><p>Menunggu peserta lain bergabung...</p></div>
         }
       </div>
-      {remoteTrackRaw && localTrackRaw && <DraggablePip trackRef={pipTrack} onTap={() => setSwapped(!swapped)} />}
+      {!isPipMode && remoteTrackRaw && localTrackRaw && <DraggablePip trackRef={pipTrack} onTap={() => setSwapped(!swapped)} />}
     </div>
   );
 }
